5987cac063c4efa8cb1206ca552b9407df5663f9
Nanzhe Zhao <zhaonanzhe@xiaomi.com>
2026-08-26 21:09:12 +0800

    f2fs: fix zeroing paths for large folios
    Several f2fs zeroing paths still use PAGE_SIZE based offsets after
    a file mapping can contain large folios.  This is fine for order-0
    folios, but it zeros the wrong range once the target block is not at
    offset 0 in a large folio.
    
    Use offset_in_folio() to translate the file block index to the folio
    offset before zeroing data in truncate_partial_data_page(), fill_zero(),
    and f2fs_get_new_data_folio().
    
    Signed-off-by: Nanzhe Zhao <zhaonanzhe@xiaomi.com>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1547,9 +1547,16 @@ struct folio *f2fs_get_new_data_folio(struct inode *inode,
 		goto got_it;
 
 	if (dn.data_blkaddr == NEW_ADDR) {
-		folio_zero_segment(folio, 0, folio_size(folio));
-		if (!folio_test_uptodate(folio))
+		size_t off = offset_in_folio(folio,
+					(loff_t)index << PAGE_SHIFT);
+
+		folio_zero_segment(folio, off, off + PAGE_SIZE);
+		if (folio_test_large(folio)) {
+			f2fs_ffs_find_or_alloc(folio);
+			f2fs_ffs_mark_subrange_uptodate(folio, off, PAGE_SIZE);
+		} else if (!folio_test_uptodate(folio)) {
 			folio_mark_uptodate(folio);
+		}
 	} else {
 		f2fs_folio_put(folio, true);
 
@@ -2635,7 +2642,7 @@ static bool __ffs_mark_subrange_uptodate(struct folio *folio,
 	return bitmap_full(ffs->state, nr_subpages);
 }
 
-static void f2fs_ffs_mark_subrange_uptodate(struct folio *folio, size_t offset,
+void f2fs_ffs_mark_subrange_uptodate(struct folio *folio, size_t offset,
 				       size_t len)
 {
 	struct f2fs_folio_state *ffs;
